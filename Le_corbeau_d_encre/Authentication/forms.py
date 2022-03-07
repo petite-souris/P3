e139ca833f9cca4f74892b64d9fc53bd8eb10fac
petite-souris <cecilia.conquet@gmail.com>
2022-03-07 16:12:00 +0100

    Delete file Authentication : failure.

--- Le_corbeau_d_encre/Authentication/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-
-class ConnexionForm(forms.Form):
-    username = forms.CharField(label="Nom d'utilisateur", max_length=30)
-    # forms.PasswordInput permit to character to stay hidden:
-    password = forms.CharField(label="Mot de passe", widget=forms.PasswordInput)
