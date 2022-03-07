d997aed3ed8782eacdee47b76f9101db046ddd3c
petite-souris <cecilia.conquet@gmail.com>
2022-03-07 13:45:46 +0100

    Views for the home page and Volva page.

--- Le_corbeau_d_encre/Okohorn/views.py
+++ Le_corbeau_d_encre/Okohorn/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'Okohorn/home.html')
+
+
+def volva(request):
+    return render(request, 'Okohorn/volva.html')
