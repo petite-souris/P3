027a234fc030179ce6dbf6df1e99401b0ee4d110
petite-souris <cecilia.conquet@gmail.com>
2022-03-07 14:26:07 +0100

    Model test.

--- Le_corbeau_d_encre/Okohorn/models.py
+++ Le_corbeau_d_encre/Okohorn/models.py
@@ -1,7 +1,5 @@
-from decimal import MAX_EMAX
 from django.db import models
 
 
-class test(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField(max_length=500)
+class test2(models.Model):
+    names = models.CharField(max_length=50)
