7b27d3d1e87ee4aa72e8d0f5f337f531f1e0c220
petite-souris <cecilia.conquet@gmail.com>
2022-04-10 14:50:09 +0200

    Modification CSS for home page.

--- Le_corbeau_d_encre/Okohorn/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from Okohorn.models import test2
-
-# Register your models here.
-
-admin.site.register(test2)
--- Le_corbeau_d_encre/Okohorn/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-
-class test2(models.Model):
-    names = models.CharField(max_length=50)
