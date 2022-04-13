6bc98b7b9e2840b4a2bb919ff55d7ed16fc24a56
petite-souris <cecilia.conquet@gmail.com>
2022-04-13 12:02:07 +0200

    Modification of the form in authentication.

--- Le_corbeau_d_encre/authentication/forms.py
+++ Le_corbeau_d_encre/authentication/forms.py
@@ -4,12 +4,6 @@ from django.contrib.auth.forms import UserCreationForm
 
 # Creation of the form to login :
 class LoginForm(forms.Form):
-    last_name = forms.CharField(required=True)
-    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True, label="Nom de famille")
+    first_name = forms.CharField(required=True, label="Prénom")
     password1 = forms.CharField(widget=forms.PasswordInput(), label='Mot de passe')
-
-# Creation of the form to new inscription :
-class NewCustomForm(forms.Form):
-    class Meto(UserCreationForm.Meta):
-        model = get_user_model()
-        fields = ('gender', 'first_name', 'last_name', 'birthday', 'adress', 'email', 'phone')
