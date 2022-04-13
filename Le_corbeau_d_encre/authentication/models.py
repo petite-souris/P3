c5b6cdcbd3fb7c6ceff359f01db7ec5b0a644a3b
petite-souris <cecilia.conquet@gmail.com>
2022-04-13 22:53:07 +0200

    Creation of an User in model.

--- Le_corbeau_d_encre/authentication/models.py
+++ Le_corbeau_d_encre/authentication/models.py
@@ -2,12 +2,16 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    
     TATTOIST = 'TATTOIST'
     CUSTOMER = 'CUSTOMER'
-
     ROLE_CHOICES = (
-        (TATTOIST, 'Tattoist-user'),
-        (CUSTOMER, 'Customer-user'),
+        (CUSTOMER, 'Client'),
+        (TATTOIST, 'Tatoueur'),
         )
     role = models.CharField(max_length=30, choices=ROLE_CHOICES, verbose_name='Rôle')
+
+    date_of_birth = models.DateField(verbose_name='Date de naissance')
+    email = models.EmailField(verbose_name='Adresse email')
+    gender = models.CharField(max_length=30, verbose_name='Genre')
+
+    REQUIRED_FIELDS = ['date_of_birth', 'email', 'gender']
