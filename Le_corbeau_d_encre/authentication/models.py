87e137167ab40d39418798a671f9e17bf24277eb
petite-souris <cecilia.conquet@gmail.com>
2022-04-13 14:36:42 +0200

    Modification of roles : tattoist or customer.

--- Le_corbeau_d_encre/authentication/models.py
+++ Le_corbeau_d_encre/authentication/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 class User(AbstractUser):
     
-    PREMIUM = 'PREMIUM'
-    FREE = 'FREE'
+    TATTOIST = 'TATTOIST'
+    CUSTOM = 'CUSTOM'
 
     ROLE_CHOICES = (
-        (PREMIUM, 'Premium-user'),
-        (FREE, 'Free-user'),
+        (TATTOIST, 'Tattoist-user'),
+        (CUSTOM, 'Custom-user'),
         )
     role = models.CharField(max_length=30, choices=ROLE_CHOICES, verbose_name='Rôle')
