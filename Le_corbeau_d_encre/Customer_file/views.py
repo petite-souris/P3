6513d8b7d8e1e59bbc02852f896afada14c6af4c
petite-souris <cecilia.conquet@gmail.com>
2022-04-20 10:19:29 +0200

    Add views for template.

--- Le_corbeau_d_encre/Customer_file/views.py
+++ Le_corbeau_d_encre/Customer_file/views.py
@@ -16,3 +16,15 @@ def user_page(request):
 def choices(request):
     css = "Customer_file/css/choices.css"
     return render(request, 'Customer_file/choices.html', {"css":css})
+
+def new_project(request):
+    css = "Customer_file/css/new_project.css"
+    return render(request, 'Customer_file/new_project.html', {"css":css})
+
+def height(request):
+    css = "Customer_file/css/height.css"
+    return render(request, 'Customer_file/height.html', {"css":css})
+
+def style(request):
+    css = "Customer_file/css/style.css"
+    return render(request, 'Customer_file/style.html', {"css":css})
